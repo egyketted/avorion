77d954e9ab33b18c477f02c751796761ada7ec58
Szabolcs Foti <szabolcs_foti@epam.com>
2017-03-03 15:25:48 +0100

    add execution condition to findAvailableFighter() to reduce server load

--- scripts/entity/ai/carriercommandmine.lua
+++ scripts/entity/ai/carriercommandmine.lua
@@ -1,10 +1,12 @@
 
 local avalibleFighter = nil
 local minableAsteroid = nil
+local asteroidToMiningFighterCount = {}
+local asteroidToMiningFighter = {}
 
 -- this function will be executed every frame on the server only
 function updateServer(timeStep)
-	--Start looking for a asteroids to assign fighters to.
+    --Start looking for a asteroids to assign fighters to.
     updateMining(timeStep)
 
 end
@@ -12,47 +14,84 @@ end
 --Assign a fighter to the asteroid.
 function findAvailableFighter()
     local sector = Sector()
-	local ship = Entity()
-	local player = Player()
-	
+    local ship = Entity()
+    local player = Player()
+    
+    local asteroids = {sector:getEntitiesByType(EntityType.Asteroid)}
+    if asteroids[1] == nil then
+        asteroidToMiningFighterCount = {}
+        return
+    end
+    
     avalibleFighter = nil
     local fighters = {sector:getEntitiesByFaction(player.index)}
     for _, fighter in pairs(fighters) do
-		local launchedFighter = Entity(fighter.index)
-		if launchedFighter.isFighter
-		and launchedFighter.isUnarmedTurret == 1 then
-			if launchedFighter.selectedObject == nil
-			or launchedFighter.selectedObject.index == nil then
-				if minableAsteroid then	
-					launchedFighter.selectedObject = minableAsteroid 
-				end
-			end
-		end
+        local launchedFighter = Entity(fighter.index)
+        local minableAsteroid = nil
+        if launchedFighter.isFighter
+        and launchedFighter.isUnarmedTurret == 1 then
+            if launchedFighter.selectedObject == nil
+            or launchedFighter.selectedObject.index == nil then
+                local nearest = math.huge
+                --Go after rich asteroids first
+                for _, a in pairs(asteroids) do
+                    local resources = a:getMineableResources()
+                    if resources ~= nil and resources > 0 then
+                        local dist = distance2(a.translationf, launchedFighter.translationf)
+                        if dist < nearest and resources / (asteroidToMiningFighterCount[a] + 1) > 300 then
+                            nearest = dist
+                            minableAsteroid = a
+                        end
+                    end
+                end
+                if minableAsteroid then    
+                    launchedFighter.selectedObject = minableAsteroid 
+                    if asteroidToMiningFighterCount[minableAsteroid] then
+                        asteroidToMiningFighterCount[minableAsteroid] = asteroidToMiningFighterCount[minableAsteroid] + 1
+                    else
+                        asteroidToMiningFighterCount[minableAsteroid] = 1
+                    end
+                    
+                    if asteroidToMiningFighter[minableAsteroid] then
+                        push(launchedFighter, asteroidToMiningFighter[minableAsteroid])
+                    else
+                        asteroidToMiningFighter[minableAsteroid] = {launchedFighter}
+                    end
+                end
+            end
+        end
     end
+end
 
+function push(element, array)
+    local i = 1
+    while array[i] do
+        i = i + 1
+    end
+    array[i] = element
 end
 
 -- check the sector for an asteroid that can be mined.
 -- if there is one, assign minableAsteroid
 function findMinableAsteroid()
     local ship = Entity()
-    local sector = Sector()	
+    local sector = Sector()    
 
     minableAsteroid = nil
 
     local asteroids = {sector:getEntitiesByType(EntityType.Asteroid)}
     local nearest = math.huge
-	--Go after rich asteroids first
+    --Go after rich asteroids first
     for _, a in pairs(asteroids) do
-		local resources = a:getMineableResources()
+        local resources = a:getMineableResources()
         if resources ~= nil and resources > 0 then
-			local dist = distance2(a.translationf, ship.translationf)
-			if dist < nearest then
-				nearest = dist
-				minableAsteroid = a
-			end
-		end
-    end	
+            local dist = distance2(a.translationf, ship.translationf)
+            if dist < nearest then
+                nearest = dist
+                minableAsteroid = a
+            end
+        end
+    end    
 
     if minableAsteroid then
         broadcastInvokeClientFunction("setMinableAsteroid", minableAsteroid.index)
@@ -60,15 +99,18 @@ function findMinableAsteroid()
 end
 
 function updateMining(timeStep)
-
-    if not valid(minableAsteroid) then
-        findMinableAsteroid()
+    local alreadyInitialized = false
+    for asteroid, fighters in pairs(asteroidToMiningFighter) do
+        if not valid(asteroid) then
+            findAvailableFighter()
+            asteroidToMiningFighter[asteroid] = nil
+            asteroidToMiningFighterCount[asteroid] = nil
+        end
+        alreadyInitialized = true
     end
-
-	if valid(minableAsteroid) then
-		findAvailableFighter()
+    if not alreadyInitialized then
+        findAvailableFighter()
     end
-
 end
 
 function setMinableAsteroid(index)
